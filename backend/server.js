925e843c50ddbbc5381812f8e69164ded5916cc3
Viranga Weerabandara <umiduviranga8@gmail.com>
2024-08-09 19:09:38 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+
+const express = require('express')
+
+const app = express()
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
+app.get('/', (req, res) => {
+    res.json({msg: 'Welcome to the MERN Stack App'});
+})
+
+app.listen(process.env.PORT, () => {
+  console.log('Server is running on port', process.env.PORT);   
+})
+
