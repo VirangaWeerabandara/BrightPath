138fb556dc92acf9586d0520660338301035904b
Viranga Weerabandara <umiduviranga8@gmail.com>
2024-10-07 15:52:08 +0530

    Add student, teacher, and course models

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,21 @@ const mongoose = require('mongoose');
 
 const app = express()
 
+const allowedOrigins = [process.env.CORS_ORIGIN, 'http://localhost:3000'];
+
+app.use(cors({
+  origin: function (origin, callback) {
+      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+          callback(null, true);
+      } else {
+          callback(new Error('Not allowed by CORS'));
+      }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+}));
+
 app.use((req, res, next) => {
-    console.log(req.path, req.method)
+    console.log(req.path, res.path)
     next()
 })
 
@@ -15,7 +28,15 @@ app.get('/', (req, res) => {
     res.json({msg: 'Welcome to the MERN Stack App'});
 })
 
-app.listen(process.env.PORT, () => {
-  console.log('Server is running on port', process.env.PORT);   
-})
+// connect to mongodb
+mongoose.connect(process.env.MONGO_URI)
+    .then(()=>{
+        // listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log('Connected to DB and listening on port', process.env.PORT);
+        })
+    })
+    .catch(err => {
+        console.log(err)
+    })
 
