30bde137b5e2f527d66395adb860ccbd3d8b5995
VirangaWeerabandara <umiduviranga8@gmail.com>
2026-01-01 16:04:24 +0530

    Refactor Dockerfile for improved build clarity and organization; remove unnecessary comments in TeacherDashboardLayout for cleaner code

--- frontend/src/components/layout/TeacherDashboardLayout.tsx
+++ frontend/src/components/layout/TeacherDashboardLayout.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Sidebar } from "../Sidebar";
-import { PageTransition } from "../pageTransition";
 
 interface DashboardLayoutProps {
   children: React.ReactNode;
@@ -10,9 +9,7 @@ export const DashboardLayout = ({ children }: DashboardLayoutProps) => {
   return (
     <div className="flex min-h-screen bg-gray-50">
       <Sidebar />
-      <main className="ml-20 flex-1 p-8 transition-all duration-300 lg:ml-64">
-        <PageTransition>{children}</PageTransition>
-      </main>
+      <main className="ml-20 flex-1 p-8 lg:ml-64">{children}</main>
     </div>
   );
 };
